db542c83ec172c59f1d22f4c5d495a655560f538
breadnet <breadnet@users.noreply.github.com>
2026-06-25 15:47:39 -0300

    Actualización: diseño moderno, nuevos productos, fotos, WhatsApp y redes sociales

---
+++ javascripts/all.js
@@ -0,0 +1,12 @@
+$(function() {
+  $('.carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+});
